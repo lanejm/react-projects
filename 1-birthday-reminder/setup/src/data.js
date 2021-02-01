b081e5c70bcdecd6579656bf250bdd0efc16067d
Claire Lane <clairelane@Claires-MacBook-Air-2.local>
2021-02-01 18:09:28 -0500

    birthday reminder

---
+++ 1-birthday-reminder/setup/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Caroline Stevens',
+    age: 31,
+    image:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  },
+  {
+    id: 2,
+    name: 'Jada Morgan',
+    age: 36,
+    image:
+      'https://images.unsplash.com/photo-1597586124394-fbd6ef244026?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    id: 3,
+    name: 'David Mason',
+    age: 28,
+    image:
+      'https://images.unsplash.com/photo-1548946526-f69e2424cf45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    id: 4,
+    name: 'Cillian Bauer',
+    age: 35,
+    image:
+      'https://images.unsplash.com/photo-1548197001-ca380947be91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    id: 5,
+    name: 'Doris Thatcher',
+    age: 37,
+    image:
+      'https://vignette.wikia.nocookie.net/bloodandicecreamtrilogy/images/6/61/70530-27300.gif/revision/latest/scale-to-width-down/180?cb=20100820132939',
+  },
+];
